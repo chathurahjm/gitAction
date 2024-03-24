3795f19560d6dbc398a4acd19aecbcc7175ca5db
chathurahjm <chathudagr8@yahoo.com>
2024-03-24 10:13:42 +0530

    Update GoogleNavigation.java

--- src/test/java/org/example/GoogleNavigation.java
+++ src/test/java/org/example/GoogleNavigation.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By; 
-
+ 
 @Test
 public class GoogleNavigation {
 
